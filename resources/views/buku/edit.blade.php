49822761cc363005da3ddd0247d61953e698ca64
Danu-Wicaksono <danuw482@gmail.com>
2022-03-16 20:43:33 +0700

    boostrap pada tampilan home

--- resources/views/buku/edit.blade.php
+++ resources/views/buku/edit.blade.php
@@ -2,46 +2,46 @@
 @section('title', 'Tambah Buku')
 @section('container')
 <div class="container">
-    <div class="row ">
-        <div class="col-10">
-            <h2 class="mt-3">Edit Data Buku</h2>
-            <form method="POST" action="{{ url ('buku/'.$buku->id) }}">
-                @csrf
-                <input type="hidden" name="_method" value="PATCH">
-                <div class="card" style="width: 30rem;">
-                <div class="card-body">
-                <div class="form-group">
-                  <label for="judul">Judul Buku</label>
-                  <input type="text" name ="judul_buku" class="form-control" id="judul" value="{{ $buku->judul_buku }}" required>
-                </div>
-                
-                <div class="form-group">
-                    <label for="penulis">Penulis</label>
-                    <input type="text" name ="penulis" class="form-control" id="penulis"  value="{{ $buku->penulis }}" required>
-                   
-                </div>
+  <div class="row ">
+    <div class="col-10">
+      <h2 class="mt-3">Edit Data Buku</h2>
+      <form method="POST" action="{{ url ('buku/'.$buku->id) }}">
+        @csrf
+        <input type="hidden" name="_method" value="PATCH">
+        <div class="card" style="width: 30rem;">
+          <div class="card-body">
+            <div class="form-group">
+              <label for="judul">Judul Buku</label>
+              <input type="text" name="judul_buku" class="form-control" id="judul" value="{{ $buku->judul_buku }}" required>
+            </div>
 
-                <div class="form-group">
-                    <label for="penerbit">Penerbit</label>
-                    <input type="text" name ="penerbit" class="form-control" id="penerbit"   value="{{ $buku->penerbit }}" required>
-                   
-                </div>
+            <div class="form-group">
+              <label for="penulis">Penulis</label>
+              <input type="text" name="penulis" class="form-control" id="penulis" value="{{ $buku->penulis }}" required>
 
-                <div class="form-group">
-                    <label for="tahun">Tahun Terbit</label>
-                    <input type="text" name ="tahun_terbit" class="form-control" id="tahun"   value="{{ $buku->tahun_terbit }}" required>
+            </div>
+
+            <div class="form-group">
+              <label for="penerbit">Penerbit</label>
+              <input type="text" name="penerbit" class="form-control" id="penerbit" value="{{ $buku->penerbit }}" required>
+
+            </div>
+
+            <div class="form-group">
+              <label for="tahun">Tahun Terbit</label>
+              <input type="text" name="tahun_terbit" class="form-control" id="tahun" value="{{ $buku->tahun_terbit }}" required>
+
+            </div>
+            <div class="form-group">
+              <label for="stok">Stok</label>
+              <input type="text" name="stok" class="form-control" id="stok" value="{{ $buku->stok }}"" required>
                    
                   </div>
-                  <div class="form-group">
-                    <label for="stok">Stok</label>
-                    <input type="text" name ="stok" class="form-control" id="stok"   value="{{ $buku->stok }}"" required>
-                   
-                  </div>
-                <button type="submit" class="btn btn-primary ">Simpan</button>
-              </form>
-        </div>
-        </div>
-        </div>
+                <button type=" submit" class="btn btn-primary ">Simpan</button>
+      </form>
     </div>
+  </div>
+</div>
+</div>
 </div>
 @endsection
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,12 +1,29 @@
 @extends('layout/main')
 
-@section('title', 'index')
+@section('title', 'Home')
 
 @section('container')
-<div class="container">
-    <div class="row">
-        <div class="col-10"></div>
-        <h1 class="mt-2">Selamat Datang di perpustakaan Politeknik Negeri Jember</h1>
+<div class="row">
+    <div class="col-sm-6">
+        <div class="card">
+            <div class="card-body">
+                <img src="{{URL::asset('/img/polije2.png')}}" class="card-img-top" alt="...">
+                <h5 class="card-title">SELAMAT DATANG DI WEBSITE PERPUSTAKAAN POLITEKNIK NEGERI JEMBER</h5>
+                <p class="card-text">Politeknik Negeri Jember merupakan perguruan tinggi yang menyelenggarakan pendidikan vokasional, yaitu program pendidikan yang mengarah proses belajar mengajar pada tingkat keahlian, keterampilan.</p>
+                <a href="https://polije.ac.id/" class="btn btn-primary">Selengkapnya</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-6">
+        <div class="card">
+            <div class="card-body">
+                <img src="{{URL::asset('/img/culture.png')}}" class="card-img-top" alt="...">
+                <h5 class="card-title">DIGITAL CULTURE</h5>
+                <p class="card-text">Digital culture yaitu kemampuan individu dalam membaca, menguraikan, membiasakan, memeriksa dan membangun wawasan kebangsaan, nilai Pancasila dan Bhinneka Tunggal Ika dalam kehidupan sehari-hari.</p>
+                <a href="https://www.youtube.com/watch?v=ke5FUxXURrw" class="btn btn-primary">Selengkapnya</a>
+            </div>
+        </div>
     </div>
 </div>
+
 @endsection

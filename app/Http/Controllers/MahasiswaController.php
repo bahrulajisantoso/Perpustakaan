235b27eb2d48aac12b063dc5d7d81ae246226b66
Gusde Biyan Ambarista <gusdebiyan28@gmail.com>
2022-03-16 19:24:30 +0700

    membenarkan eror

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -36,7 +36,7 @@ class MahasiswaController extends Controller
      */
     public function create()
     {
-        return view ('mahasiswa.create');
+        return view('mahasiswa.create');
     }
 
     /**
@@ -88,7 +88,13 @@ class MahasiswaController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $mahasiswa = Student::find($id);
+        $mahasiswa->nim = $request->nim;
+        $mahasiswa->nama = $request->nama;
+        $mahasiswa->jurusan = $request->jurusan;
+        $mahasiswa->save();
+
+        return redirect()->route('mahasiswa.index');
     }
 
     /**
